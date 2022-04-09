2681740b7314b2d450ca88634d4e6835fd05dfb8
amata1219 <suzukiamata@gmail.com>
2022-04-09 23:05:04 +0900

    ブロックチェーンを実装

---
+++ src/main/java/amata1219/simpleblockchain/blockchain/Blockchain.java
@@ -0,0 +1,36 @@
+package amata1219.simpleblockchain.blockchain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Blockchain {
+
+    public final List<Block> chain;
+    public List<Transaction> currentTransactions;
+
+    public Blockchain() {
+        chain = new ArrayList<>();
+        currentTransactions = new ArrayList<>();
+
+        //make the genesis block
+        newBlock(100, "1");
+    }
+
+    public Block newBlock(int proof, String previousHash) {
+        Block block = new Block(chain.size() + 1, System.currentTimeMillis(), currentTransactions, proof, previousHash);
+        currentTransactions = new ArrayList<>();
+        chain.add(block);
+        return block;
+    }
+
+    public Block lastBlock() {
+        return chain.get(chain.size() - 1);
+    }
+
+    //return the index of the block containing this transaction
+    public int newTransaction(String sender, String recipient, int amount) {
+        currentTransactions.add(new Transaction(sender, recipient, amount));
+        return lastBlock().index() + 1;
+    }
+
+}

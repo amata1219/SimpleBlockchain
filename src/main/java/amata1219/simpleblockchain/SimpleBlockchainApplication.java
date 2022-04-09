ec523e616b350078cad257da7901f83c21f51945
amata1219 <suzukiamata@gmail.com>
2022-04-09 22:59:59 +0900

    initial commit

---
+++ src/main/java/amata1219/simpleblockchain/SimpleBlockchainApplication.java
@@ -0,0 +1,13 @@
+package amata1219.simpleblockchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBlockchainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBlockchainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/amata1219/simpleblockchain/SimpleBlockchainApplicationTests.java
@@ -0,0 +1,13 @@
+package amata1219.simpleblockchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBlockchainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
